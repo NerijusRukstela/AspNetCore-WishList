e8a6905802d13f46643e346a477c97eb21defcf7
Nerijus Rukštela <nerijus.rukstela@itvision.lt>
2018-06-06 06:50:46 +0300

    Create home folder and controllers
    Create Home Views and the HomeController

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+    }
+}
